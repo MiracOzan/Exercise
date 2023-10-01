70c21eac0d092fc2401f63b4edbac5ad3be3f216
Miraç Ozan T <m.ozan.tosun@gmail.com>
2023-10-01 12:32:37 +0300

    update(Auth)

--- src/WebApi/Controllers/AuthController.cs
+++ src/WebApi/Controllers/AuthController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using WebApi.Data;
 using WebApi.Dto;
 using WebApi.Models;
@@ -18,11 +22,11 @@ public class AuthController : Controller
     }
 
     [HttpPost("register")]
-    public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
+    public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto)
     {
         if (await _authRepository.UserExist(userForRegisterDto.UserName))
         {
-            ModelState.AddModelError("Username","Username already Exist");
+            ModelState.AddModelError("Username", "Username already Exist");
         }
 
         if (!ModelState.IsValid)
@@ -39,4 +43,33 @@ public class AuthController : Controller
         return StatusCode(201);
     }
 
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] UserForLoginDto userForLoginDto)
+    {
+        var user = _authRepository.Login(userForLoginDto.UserName, userForLoginDto.Password);
+
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        var tokenhandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Token").Value);
+        
+        var tokenDescriptior = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, userForLoginDto.UserName)
+            }),
+            Expires = DateTime.Now.AddDays(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+        };
+
+        var Token = tokenhandler.CreateToken(tokenDescriptior);
+        var TokenString = tokenhandler.WriteToken(Token);
+
+        return Ok(TokenString);
+    }
 }    
--- src/WebApi/Models/User.cs
+++ src/WebApi/Models/User.cs
@@ -12,6 +12,6 @@ public class User
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
 
-    public List<City> Citys { get; set; }
+    public List<City> Citys { get; set; } 
 }
     
